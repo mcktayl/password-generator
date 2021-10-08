c90f2c38d42323454d1e9a4158fabd5a9d84cc1e
McKenzie Cottrell <mckenziecottrell@gmail.com>
2021-10-07 20:48:13 -0400

    Added function to randomly generate a password of length 8-128 when prompted -- currently all lowercase.

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,25 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  // Variables for character sets
+  var numbers = "1234567890";
 
+  function generatePassword() {
+    var confirmLength = prompt("Please choose a length between 8 and 128 characters.");
+    
+    if (confirmLength < 8 || confirmLength > 128) {
+      alert("Please choose a valid length.");
+    
+    } else {
+    alpha = "abcdefghijklmnopqrstuvwxyz",
+    retVal = "";
+
+    for (var i = 0, n = alpha.length; i < confirmLength; i++) {
+      retVal += alpha.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+  }
+}
 }
 
 // Add event listener to generate button
